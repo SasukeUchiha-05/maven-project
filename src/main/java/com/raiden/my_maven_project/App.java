8ea6eec0d1a69511252cb84f9864db539cf1f8f6
SasukeUchiha-05 <147145960+SasukeUchiha-05@users.noreply.github.com>
2024-12-04 14:41:31 +0530

    Update App.java

--- src/main/java/com/raiden/my_maven_project/App.java
+++ src/main/java/com/raiden/my_maven_project/App.java
@@ -5,6 +5,6 @@ package com.raiden.my_maven_project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! hello b*tches!!");
     }
 }
